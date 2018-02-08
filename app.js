240d2b98e39b976d37dc3914c00dee430130561b
vinayp321 <vinprakash@deloitte.com>
2018-02-08 20:39:00 +0530

    build test

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-let dbConnector = require('./db/connector');
-import Forum from './db/schema/forum';
+//let dbConnector = require('./db/connector');
+//import Forum from './db/schema/forum';
 let express = require('express');
 let app= express();
 
-dbConnector.connect();
-app.get('/',(req,res) => {
-  let data={
-    question:'String',
-    topic:'String',
-    askedBy:'String',
-    dateTime:null,
-    answer:[{
-      id:'String',
-      answer:'String',
-      answerBy:'String',
-      dateTime:null
-    }]
-  };
-  Forum.create(data,(err, data) => {
-    if(err){
-      res.json('error');
-    }
-    res.json(data);
-  });
+//dbConnector.connect();
+ app.get('/',(req,res) => {
+//   let data={
+//     question:'String',
+//     topic:'String',
+//     askedBy:'String',
+//     dateTime:null,
+//     answer:[{
+//       id:'String',
+//       answer:'String',
+//       answerBy:'String',
+//       dateTime:null
+//     }]
+//   };
+//   Forum.create(data,(err, data) => {
+//     if(err){
+//       res.json('error');
+//     }
+//     res.json(data);
+//   });
+res.send('success');
 
 })
 app.listen(1111,() => {
