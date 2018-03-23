9abaaf84c6276a70ff39825869061c007b471643
vinayp321 <vinprakash@deloitte.com>
2018-03-23 10:59:31 +0530

    deployment

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let dbConnector = require('./db/connector');
-import Forum from './db/schema/forum';
+//let dbConnector = require('./db/connector');
+//import Forum from './db/schema/forum';
 let express = require('express');
 let app= express();
 var path    = require("path");
@@ -18,29 +18,29 @@ app.get('/test',function(req,res){
   res.send('Test Screen');
   //__dirname : It will resolve to your project folder.
 });
-dbConnector.connect();
- app.get('/',(req,res) => {
-  let data={
-    question:'String',
-    topic:'String',
-    askedBy:'String',
-    dateTime:null,
-    answer:[{
-      id:'String',
-      answer:'String',
-      answerBy:'String',
-      dateTime:null
-    }]
-  };
-  Forum.create(data,(err, data) => {
-    if(err){
-      res.json('error');
-    }
-    console.log('2232')
-    res.json(data);
-  });
-//res.sendFile('./index.html');
-})
+// dbConnector.connect();
+//  app.get('/',(req,res) => {
+//   let data={
+//     question:'String',
+//     topic:'String',
+//     askedBy:'String',
+//     dateTime:null,
+//     answer:[{
+//       id:'String',
+//       answer:'String',
+//       answerBy:'String',
+//       dateTime:null
+//     }]
+//   };
+//   Forum.create(data,(err, data) => {
+//     if(err){
+//       res.json('error');
+//     }
+//     console.log('2232')
+//     res.json(data);
+//   });
+// //res.sendFile('./index.html');
+// })
 app.listen(PORT,() => {
   console.log(`Example app listening on port ${PORT}`)
 })
