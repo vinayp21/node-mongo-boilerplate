ee63cbefddeed97f6f7faaf341c67ad1ac243dc3
vinayp321 <vinprakash@deloitte.com>
2018-03-23 11:49:12 +0530

    deployment

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 let express = require('express');
 let app= express();
 var path    = require("path");
-var multer= require('multer');
-var upload= multer({dest:'./uploads'})
+//var multer= require('multer');
+//var upload= multer({dest:'./uploads'})
 
 let PORT=process.env.PORT || 1111
-app.post('/upload', upload.single('test'), function(req, res, next){
-
-})
+// app.post('/upload', upload.single('test'), function(req, res, next){
+//
+// })
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
